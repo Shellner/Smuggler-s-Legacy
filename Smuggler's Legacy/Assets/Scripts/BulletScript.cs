2f169af7de46fd7f4fa727e318d93b0d19b9a065
Shellner <at@shellner.com>
2018-04-09 13:22:52 +0200

    First prototype 2018-04-09
    First work on the project

---
+++ Smuggler's Legacy/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+
+    public float velocityX = 5.0f;
+    public float velocityY = 0;
+     public float timer;
+    public GameObject bullet;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        rb.velocity = new Vector2(velocityX, velocityY);
+        
+       // Destroy(gameObject, 3f);
+	}
+
+}
---
+++ Smuggler's Legacy/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    public GameObject instance;
+    public float velocityX = 1.2f;
+    public float velocityY = 2.0f;
+    public GameObject bullet;
+    Vector2 bulletPos;
+    public float fireRate = 0.5f;
+    public float nextFire = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime * velocityX;
+
+        if (Input.GetKey("w"))
+        {
+            transform.position += (Vector3.up * velocityY * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += (Vector3.down * velocityY * Time.deltaTime);
+        }
+
+        if (Input.GetKey("h") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            fire();
+        }
+
+    }
+    void fire(){
+        bulletPos = transform.position;
+        bulletPos += new Vector2(1.2f, 0.0f);
+         instance = Instantiate(bullet, bulletPos, Quaternion.identity)as GameObject;
+        Destroy(instance, 3.0f);
+    }
+}
