13cb1d9fb10006b50886d8e440abf7af41973deb
SilverVarg <37655905+SilverVarg@users.noreply.github.com>
2018-04-20 12:58:25 +0200

    Main Menu

--- Smuggler's Legacy/Assets/Scripts/MenuController.cs
+++ Smuggler's Legacy/Assets/Scripts/MenuController.cs
@@ -10,6 +10,11 @@ public void StartGame()
         SceneManager.LoadScene(1);
     }
 
+    public void ShowCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
 public void QuitGame()
     {
         Application.Quit();
